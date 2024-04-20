0cdd7602fa58a84833a74921c7f80e0bc9c02ba3
chandyego <chandler.juego@wsu.edu>
2024-04-19 19:46:03 -0700

    Connecting to MongoDB

---
+++ config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const db = 
+"mongodb+srv://chandlerjuego101:LdjMdyRv9DqjUwjd@cpts322-libraryassistan.uo25qpa.mongodb.net/";
+
+mongoose.set("strictQuery", true, "useNewUrlParser", true);
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(db);
+        console.log("MongoDB is connected...");
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
