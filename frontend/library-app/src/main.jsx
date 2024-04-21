da0e57f868145d9be626beb4af077ef3c043ef96
chandyego <chandler.juego@wsu.edu>
2024-04-21 16:17:06 -0700

    Frontend for very basic CRUd for books (no user profiles implemented at all), no API for google books

--- frontend/library-app/src/main.jsx
+++ frontend/library-app/src/main.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 // components
-import CreateBook from "./components/CreateBook.js";
-import ShowBookList from "./components/ShowBookList.js";
+import CreateBook from "./components/CreateBook.jsx";
+import ShowBookList from "./components/ShowBookList.jsx";
 import ShowBookDetails from "./components/ShowBookDetails";
 import UpdateBookInfo from "./components/UpdateBookInfo";
 
-
-// Routes
-const router = createBrowserRouter([
-  { path: "/", element: <ShowBookList /> },
-  { path: "/create-book", element: <CreateBook /> },
-  { path: "/show-book/:id", element: <ShowBookDetails /> },
-  { path: "/edit-book/:id", element: <UpdateBookInfo /> },
-])
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ShowBookList />} />
+        <Route path="/create-book" element={<CreateBook />} />
+        <Route path="/show-book/:id" element={<ShowBookDetails />} />
+        <Route path="/edit-book/:id" element={<UpdateBookInfo />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
--- frontend/library-app/vite.config.js
+++ frontend/library-app/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    extensions: ['.js', '.jsx'] // Add JSX file extension resolution
+  },
+  esbuild: {
+    loader: 'jsx', // Set the esbuild loader to handle JSX syntax
+  },
+});
